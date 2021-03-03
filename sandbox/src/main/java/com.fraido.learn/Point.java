cd1031a00a2e1133bd55ce5682a92694bef4e76f
ILoveBeerVeryMuch <javalearn@beerbeardbar.ru>
2021-03-03 20:37:16 +0300

    fix comments

--- sandbox/src/main/java/com.fraido.learn/Point.java
+++ sandbox/src/main/java/com.fraido.learn/Point.java
@@ -4,16 +4,13 @@ public class Point {
     private double x;
     private double y;
 
-    public Point() {
-
-    }
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow(p1.x-p2.x, 2)+Math.pow(p1.y-p2.y, 2));
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow(this.x-p.x, 2)+Math.pow(this.y-p.y, 2));
     }
 
 }
