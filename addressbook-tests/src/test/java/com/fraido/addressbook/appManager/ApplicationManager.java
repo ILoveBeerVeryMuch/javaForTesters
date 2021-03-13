b648e5069fc942cae5885996242ab6b7340d9df0
ILoveBeerVeryMuch <javalearn@beerbeardbar.ru>
2021-03-13 22:57:04 +0300

    add capability to run tests in different browsers

--- addressbook-tests/src/test/java/com/fraido/addressbook/appManager/ApplicationManager.java
+++ addressbook-tests/src/test/java/com/fraido/addressbook/appManager/ApplicationManager.java
@@ -1,11 +1,10 @@
 package com.fraido.addressbook.appManager;
 
-import com.fraido.addressbook.model.PersonData;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,9 +15,20 @@ public class ApplicationManager {
     private GroupHelper groupsHelper;
     private PersonHelper personHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else {
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupsHelper = new GroupHelper(wd);
--- addressbook-tests/src/test/java/com/fraido/addressbook/tests/BaseTest.java
+++ addressbook-tests/src/test/java/com/fraido/addressbook/tests/BaseTest.java
@@ -1,12 +1,13 @@
 package com.fraido.addressbook.tests;
 
 import com.fraido.addressbook.appManager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-    protected final ApplicationManager applicationManager = new ApplicationManager();
+    protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
